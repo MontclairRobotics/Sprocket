1cd7509cd329be51ae6959d7320bb33bd94cfc98
Rafi Baum <rafi@ukbaums.com>
2016-04-12 17:56:08 -0400

    Made TankTranslator simpler Worked on fixing examples

--- src/org/montclairrobotics/sprocket/examples/Robot.java
+++ src/org/montclairrobotics/sprocket/examples/Robot.java
@@ -1,7 +1,6 @@
 
 package org.montclairrobotics.sprocket.examples;
 
-import org.montclairrobotics.sprocket.control.Control;
 import org.montclairrobotics.sprocket.drive.DriveTrain;
 import org.montclairrobotics.sprocket.drive.DriveTrain.M_TYPE;
 import org.montclairrobotics.sprocket.drive.Lock;
