6142bf609fb9068d473fb4d5173f65c4f5ec27b2
Rafi Baum <rafi@ukbaums.com>
2016-05-20 20:56:52 -0400

    Minor cleanup

--- src/org/montclairrobotics/sprocket/drive/DriveTrain.java
+++ src/org/montclairrobotics/sprocket/drive/DriveTrain.java
@@ -3,21 +3,13 @@ package org.montclairrobotics.sprocket.drive;
 import org.montclairrobotics.sprocket.drive.Motor.M_TYPE;
 import org.montclairrobotics.sprocket.utils.Angle;
 import org.montclairrobotics.sprocket.utils.Degree;
-import org.montclairrobotics.sprocket.utils.Input;
 import org.montclairrobotics.sprocket.utils.PID;
 import org.montclairrobotics.sprocket.utils.Polar;
 import org.montclairrobotics.sprocket.utils.Priority;
 import org.montclairrobotics.sprocket.utils.Updatable;
-import org.montclairrobotics.sprocket.utils.UpdateClass;
 import org.montclairrobotics.sprocket.utils.Updater;
 import org.montclairrobotics.sprocket.utils.Vector;
 import org.montclairrobotics.sprocket.utils.XY;
-
-import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.VictorSP;
 
 /**
  * The main drivetrain class which holds a set of wheels
