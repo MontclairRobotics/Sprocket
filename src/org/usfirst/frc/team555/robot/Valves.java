7cf856c45c2245540fb34cd61c05c7cd22c9818f
Rafi Baum <rafi@ukbaums.com>
2016-06-23 22:16:42 -0400

    I gave back up and went to the old auto align code

--- src/org/usfirst/frc/team555/robot/Valves.java
+++ src/org/usfirst/frc/team555/robot/Valves.java
@@ -70,11 +70,82 @@ public class Valves {
 		new Shoot(1);
 		new ShootMotorOn(3);
 		new ShootMotorIntake(2);
-		new AlignOn(11,new XY(160,200),new ParallelPID(0,0,0),new ParallelPID(-0.008,0,0));
+		new AlignOn(11,new XY(160,200));//,new ParallelPID(0,0,0),new ParallelPID(-0.008,0,0));
 		
 		raise();
 		halfOff();
 	}
+	public class AlignOn extends Button
+	{
+		private int loopsAtTarget=0;
+		private static final double 
+			BUFFER_X_SMALL=30,
+			BUFFER_X_BIG=60,
+			BUFFER_Y_SMALL=14,
+			BUFFER_Y_BIG=60,
+			TURN_SMALL=0.25,
+			TURN_BIG=0.27,
+			MOVE_SMALL=0.25,
+			MOVE_BIG=0.3,
+			BLEND=1;
+		private double x,y;
+		private XY target;
+		public AlignOn(int id,XY target) {
+			super(Control.sticks[Control.DRIVE_STICK], id);
+			this.target=target;
+		}
+		public void down()
+		{
+			x=x*(1-BLEND)+(Robot.grip.getX()-target.getX())*BLEND;
+			y=y*(1-BLEND)+(Robot.grip.getY()-target.getY())*BLEND;
+			double spd,rot;
+			boolean atTarget=false;
+			if(x==0)
+				rot=0;
+			else if(x>BUFFER_X_BIG)
+				rot=TURN_BIG;
+			else if(x>BUFFER_X_SMALL)
+				rot=TURN_SMALL;
+			else if(x<-BUFFER_X_BIG)
+				rot=-TURN_BIG;
+			else if(x<-BUFFER_X_SMALL)
+				rot=-TURN_SMALL;
+			else
+			{
+				rot=0;
+				atTarget=true;
+			}
+			if(y==0)
+				spd=0;
+			else if(y>BUFFER_Y_BIG)
+				spd=-MOVE_BIG;
+			else if(y>BUFFER_Y_SMALL)
+				spd=-MOVE_SMALL;
+			else if(y<-BUFFER_Y_BIG)
+				spd=MOVE_BIG;
+			else if(y<-BUFFER_Y_SMALL)
+				spd=MOVE_SMALL;
+			else
+			{
+				spd=0;
+				if(atTarget)
+				{
+					loopsAtTarget++;
+				}
+			}
+			Robot.driveTrain.driveSpeedRotation(rot,spd);
+			//Robot.align=true;
+		}
+		public void up()
+		{
+			//Robot.align=false;
+		}
+		public int getLoopsAtTarget()
+		{
+			return loopsAtTarget;
+		}
+	}
+	/*
 	public class AlignOn extends Button
 	{
 		private int i=0;
@@ -107,7 +178,7 @@ public class Valves {
 		public void up()
 		{
 		}
-	}
+	}*/
 	public class ShootMotorOn extends Button
 	{
 
