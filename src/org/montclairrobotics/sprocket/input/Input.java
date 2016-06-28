5acb054f7f9e59a3f1a173dc34d6e62f29c6f5a2
hooper-dylan <jack.hymowitz@gmail.com>
2016-06-28 19:28:16 -0400

    Input documentation

--- src/org/montclairrobotics/sprocket/input/Input.java
+++ src/org/montclairrobotics/sprocket/input/Input.java
@@ -4,20 +4,44 @@ import org.montclairrobotics.sprocket.SprocketObj;
 import org.montclairrobotics.sprocket.updater.Priority;
 import org.montclairrobotics.sprocket.updater.Updatable;
 import org.montclairrobotics.sprocket.updater.Updater;
-
+/**
+ * The idea behind this implementation 
+ * is that all inputs are read at the same time.
+ * This is similar to Cyborg's implementation of input mappers, 
+ * but it sacrifices structure and efficiency for 
+ * ease of use and simplicity.
+ * Each input may be read more than once, but all inputs are read
+ * before the rest of the calculations at the same time.
+ * @author MontclairRobotics
+ *
+ */
 public abstract class Input extends SprocketObj implements Updatable{
 	private double value;
 	public Input()
 	{
 		Updater.add(this, Priority.INPUT);
 	}
+	/**
+	 * The function to get the current value of the input;
+	 * @return the value of the input as a double
+	 */
 	public final double get()
 	{
 		return value;
 	}
+	/**
+	 * Called automatically to refresh the input 
+	 * at the beginning of each loop
+	 */
 	public final void update()
 	{
 		value=getInput();
 	}
+	/**
+	 * This function is called to read the input, and is defined
+	 * by the user. It must return the input at the time the function
+	 * is called.
+	 * @return the input
+	 */
 	public abstract double getInput();
 }
