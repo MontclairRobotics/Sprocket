4b8accd0dbc9637508cde5fc23e31e74d7960eab
Rafi Baum <rafi@ukbaums.com>
2016-12-16 22:42:30 -0500

    Started work on motor

---
+++ src/org/montclairrobotics/sprocket/drive/Motor.java
@@ -0,0 +1,20 @@
+package org.montclairrobotics.sprocket.drive;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedController;
+import org.montclairrobotics.sprocket.utils.PID;
+
+public class Motor {
+
+    public enum MotorType {
+        CANTALON,
+        TALON
+    }
+
+
+    private SpeedController motor;
+    private Encoder enc;
+    private PID pid;
+    
+
+}
