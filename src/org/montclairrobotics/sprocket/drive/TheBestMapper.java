85117cc66b06b28aa471ea7a1969c5770d7b6580
jackhymowitz <jack.hymowitz@gmail.com>
2016-12-27 17:18:25 -0500

    More Best Mapper - Have to change the module

--- src/org/montclairrobotics/sprocket/drive/TheBestMapper.java
+++ src/org/montclairrobotics/sprocket/drive/TheBestMapper.java
@@ -1,5 +1,22 @@
 package org.montclairrobotics.sprocket.drive;
 
-public class TheBestMapper {
+import org.montclairrobotics.sprocket.geometry.Angle;
+import org.montclairrobotics.sprocket.geometry.Vector;
+
+public class TheBestMapper implements DriveTrainMapper{
+
+	@Override
+	public void map(DriveTrainTarget driveTarget, DriveModule[] driveModules) {
+		Vector tgtDirection=driveTarget.getDirection();
+		Angle tgtTurn=driveTarget.getTurn();
+		for(DriveModule module:driveModules)
+		{
+			tgtTurn=tgtTurn.add(module.setDirection(tgtDirection));
+		}
+		for(DriveModule module:driveModules)
+		{
+			module.setTurn(tgtTurn);
+		}
+	}
 
 }
