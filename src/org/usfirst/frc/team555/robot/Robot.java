20928d6eef5288981e2be278e24f713b9a8135fe
Rafi Baum <rafi@ukbaums.com>
2017-01-12 15:45:25 -0500

    Added example robot

---
+++ src/org/usfirst/frc/team555/robot/Robot.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team555.robot;
+
+import org.montclairrobotics.sprocket.SprocketRobot;
+import org.montclairrobotics.sprocket.control.ArcadeDriveInput;
+import org.montclairrobotics.sprocket.drive.DriveTrainBuilder;
+import org.montclairrobotics.sprocket.drive.InvalidDriveTrainException;
+import org.montclairrobotics.sprocket.geometry.Position;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Robot extends SprocketRobot {
+
+	@Override
+	public void robotInit() 
+	{
+		super.robotInit();
+		
+		DriveTrainBuilder builder = new DriveTrainBuilder();
+		builder
+		.addWheel(new CANTalon(0), Position.FL)
+		.addWheel(new CANTalon(1), Position.FR)
+		.addWheel(new CANTalon(2), Position.BL)
+		.addWheel(new CANTalon(3), Position.BR);
+		
+		builder.setInput(new ArcadeDriveInput(new Joystick(0)));
+		
+		try 
+		{
+			builder.build();
+		}
+		catch (InvalidDriveTrainException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void teleopInit() {
+		super.teleopInit();
+	}
+	
+	
+
+}
