3f4b924e4a50953c13a13fbd5a8c0cd815683695
jackhymowitz <jack.hymowitz@gmail.com>
2017-01-14 11:43:16 -0500

    A boring wrapper for rafi

--- src/org/montclairrobotics/sprocket/geometry/Vector.java
+++ src/org/montclairrobotics/sprocket/geometry/Vector.java
@@ -3,14 +3,14 @@ package org.montclairrobotics.sprocket.geometry;
 public interface Vector {
 
     Vector ZERO = new XY(0,0);
-	Distance getMagnitude();
+	double getMagnitude();
     Angle getAngle();
-    Distance getX();
-    Distance getY();
+    double getX();
+    double getY();
     Vector add(Vector v);
     Vector subtract(Vector v);
     Vector scale(double s,boolean norm);
-    Distance dotProduct(Vector v);
+    double dotProduct(Vector v);
 	Vector rotate(Angle angle);
 
 }
