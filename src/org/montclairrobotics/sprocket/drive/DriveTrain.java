9464984ff53678f73d60efda94704f6dc48b53da
jackhymowitz <jack.hymowitz@gmail.com>
2017-01-14 10:54:00 -0500

    Merging

--- src/org/montclairrobotics/sprocket/drive/DriveTrain.java
+++ src/org/montclairrobotics/sprocket/drive/DriveTrain.java
@@ -6,45 +6,37 @@ import java.util.List;
 import org.montclairrobotics.sprocket.loop.Priority;
 import org.montclairrobotics.sprocket.loop.Updatable;
 import org.montclairrobotics.sprocket.loop.Updater;
+import org.montclairrobotics.sprocket.utils.Input;
 
 public class DriveTrain implements Updatable {
 
-    private List<DriveModule> driveModules;
-    private DrivePipeline pipeline;
-    private DriveTrainMapper powerMapper;
-    private DriveTrainInput input;
+
+	private Input<DriveTrainTarget> input;
+	private DriveModule[] modules;
+    private DriveTrainMapper mapper;
 
 
-    public DriveTrain(DriveModule[] modules, DriveTrainInput input, DrivePipeline pipeline, DriveTrainMapper mapper) {
-    	this.driveModules = Arrays.asList(modules);
-    	this.input = input;
-    	this.pipeline = pipeline;
-    	this.powerMapper = mapper;
+    public DriveTrain(DriveModule... modules) {
+    	this.modules = modules;
     	
     	Updater.add(this, Priority.DRIVE_CALC);
     }
-
-public class Drivetrain implements Updatable{
-	private Input<DriveTrainTarget> input;
-	private DriveModule[] modules;
-	
-	public Drivetrain(DriveModule[] modules)
-	{
-		this.input=new Pipeline<DriveTrainTarget>(DriveTrainTarget.ZERO);
-		this.modules=modules;
-	}
-	
-	public void setInput(Input<DriveTrainTarget> input)
-	{
-		this.input=input;
-	}
+    
+    public DriveTrain setInput(Input<DriveTrainTarget> input)
+    {
+    	this.input=input;
+    	return this;
+    }
+    public DriveTrain setMapper(DriveTrainMapper mapper)
+    {
+    	this.mapper=mapper;
+    	return this;
+    }
 
 	@Override
 	public void update() {
-		DriveTrainTarget target = new DriveTrainTarget(input.getDirection(), input.getTurn(), input.getInputType());
-		pipeline.run(target);
-		powerMapper.map(target, driveModules.toArray(new DriveModule[]{}));
-		DriveTrainTarget target=input.get();
+		DriveTrainTarget target = input.get();
+		mapper.map(target, modules);
 	}
     
     
