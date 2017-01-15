fcf06eca502895a1ef383ac847b49e7d4315dc33
jackhymowitz <jack.hymowitz@gmail.com>
2017-01-14 22:48:15 -0500

    Implemented joystick scaling correctly

--- src/org/montclairrobotics/sprocket/control/ArcadeDriveInput.java
+++ src/org/montclairrobotics/sprocket/control/ArcadeDriveInput.java
@@ -1,7 +1,6 @@
 package org.montclairrobotics.sprocket.control;
 
-import org.montclairrobotics.sprocket.drive.DriveTrainTarget;
-import org.montclairrobotics.sprocket.drive.MotorInputType;
+import org.montclairrobotics.sprocket.drive.DTInput;
 import org.montclairrobotics.sprocket.geometry.Angle;
 import org.montclairrobotics.sprocket.geometry.Distance;
 import org.montclairrobotics.sprocket.geometry.RVector;
@@ -15,10 +14,10 @@ import org.montclairrobotics.sprocket.utils.Input;
 
 import edu.wpi.first.wpilibj.Joystick;
 
-public class ArcadeDriveInput implements Input<DriveTrainTarget>,Updatable {
+public class ArcadeDriveInput implements DTInput,Updatable {
 
     private Joystick stick;
-    MotorInputType inputType;
+    //MotorInputType inputType;
     
     //private boolean speedControl;
     private Distance maxSpeed;
@@ -28,20 +27,20 @@ public class ArcadeDriveInput implements Input<DriveTrainTarget>,Updatable {
     private Angle turn;
 
     public ArcadeDriveInput(Joystick stick) {
-    	inputType=MotorInputType.PERCENT;
+    	//inputType=MotorInputType.PERCENT;
         this.stick = stick;
         this.maxSpeed=new Distance(1);
         this.maxTurn=Angle.QUARTER;
         Updater.add(this, Priority.INPUT);
     }
 
-    public ArcadeDriveInput(Joystick stick, Distance maxSpeed, Angle maxTurn) {
+    /*public ArcadeDriveInput(Joystick stick, Distance maxSpeed, Angle maxTurn) {
         inputType=MotorInputType.SPEED;
         this.stick = stick;
         this.maxSpeed = maxSpeed;
         this.maxTurn=maxTurn;
         Updater.add(this, Priority.INPUT);
-    }
+    }*/
 
 
     public void update() {
@@ -52,14 +51,23 @@ public class ArcadeDriveInput implements Input<DriveTrainTarget>,Updatable {
     public RVector getDirection() {
         return dir;
     }
+    public RVector getDir()
+    {
+    	return getDirection();//I'm very lazy
+    }
 
     public Angle getTurn() {
         return turn;
     }
 
 	@Override
+	public DTInput.Type getInputType() {
+		return DTInput.Type.PERCENT;
+	}
+
+	/*@Override
 	public DriveTrainTarget get() {
 		// TODO Auto-generated method stub
 		return new DriveTrainTarget(getDirection(),getTurn(),inputType);
-	}
+	}*/
 }
