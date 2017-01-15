a9e76365a81674e08ca2034ec80826b6e56e79c2
jackhymowitz <jack.hymowitz@gmail.com>
2017-01-14 22:03:46 -0500

    Revert "Implemented Input Scaling"
    This reverts commit eada188ccbbaff754a2966263a980d70caf4648c.

--- src/org/montclairrobotics/sprocket/drive/DriveTrainTarget.java
+++ src/org/montclairrobotics/sprocket/drive/DriveTrainTarget.java
@@ -2,27 +2,19 @@ package org.montclairrobotics.sprocket.drive;
 
 import org.montclairrobotics.sprocket.geometry.Angle;
 import org.montclairrobotics.sprocket.geometry.RVector;
-import org.montclairrobotics.sprocket.geometry.RXY;
-import org.montclairrobotics.sprocket.geometry.Radians;
 import org.montclairrobotics.sprocket.geometry.Vector;
-import org.montclairrobotics.sprocket.geometry.XY;
 
 public class DriveTrainTarget {
 
     public static final DriveTrainTarget ZERO = new DriveTrainTarget(RVector.ZERO,Angle.ZERO,MotorInputType.SPEED);
 	private RVector direction;
     private Angle turn;
-    //private MotorInputType inputType;
+    private MotorInputType inputType;
 
     public DriveTrainTarget(RVector direction, Angle turn, MotorInputType inputType) {
         this.direction = direction;
         this.turn = turn;
-        if(inputType.equals(MotorInputType.PERCENT))
-        {
-        	direction=new RXY(direction.getX(),direction.getY(),DriveTrain.maxSpeed);
-        	turn=new Radians(turn.toRadians()*DriveTrain.maxSpeed.get());
-        }
-        //this.inputType = inputType;
+        this.inputType = inputType;
     }
 
     public RVector getDirection() {
@@ -41,7 +33,7 @@ public class DriveTrainTarget {
         this.turn = turn;
     }
 
-    /*public MotorInputType getInputType() {
+    public MotorInputType getInputType() {
         return inputType;
-    }*/
+    }
 }
