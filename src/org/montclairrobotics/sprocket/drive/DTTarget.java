f7f687f3f57083aef03983aeb053b1aec0793e23
MHSRobotics <mhs@mpsdnj.us>
2017-02-22 15:16:11 -0500

    I wrote a good commit message before but git erased it so nya.

--- src/org/montclairrobotics/sprocket/drive/DTTarget.java
+++ src/org/montclairrobotics/sprocket/drive/DTTarget.java
@@ -6,8 +6,8 @@ import org.montclairrobotics.sprocket.geometry.Vector;
 public class DTTarget {
 
     public static final DTTarget ZERO = new DTTarget(Vector.ZERO,Angle.ZERO);
-	private Vector direction;
-    private Angle turn;
+	private final Vector direction;
+    private final Angle turn;
     //private MotorInputType inputType;
 
     public DTTarget(Vector vector, Angle turn) {
@@ -20,19 +20,24 @@ public class DTTarget {
         return direction;
     }
 
-    public void setDirection(Vector direction) {
+    /*public void setDirection(Vector direction) {
         this.direction = direction;
-    }
+    }*/
 
     public Angle getTurn() {
         return turn;
     }
 
-    public void setTurn(Angle turn) {
+    /*public void setTurn(Angle turn) {
         this.turn = turn;
-    }
+    }*/
 
     /*public MotorInputType getInputType() {
         return inputType;
     }*/
+    
+    public String toString()
+    {
+    	return "Dir: "+direction+", Turn:"+turn;
+    }
 }
