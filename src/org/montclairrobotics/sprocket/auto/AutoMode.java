e9f97bb58b1642c7db61539be9b82c3c4bd83b4c
MHSRobotics <mhs@mpsdnj.us>
2017-02-28 16:18:11 -0500

    Auto Mode is now a State Machine as it should be

--- src/org/montclairrobotics/sprocket/auto/AutoMode.java
+++ src/org/montclairrobotics/sprocket/auto/AutoMode.java
@@ -3,6 +3,7 @@ package org.montclairrobotics.sprocket.auto;
 import org.montclairrobotics.sprocket.SprocketRobot;
 import org.montclairrobotics.sprocket.drive.DTInput;
 import org.montclairrobotics.sprocket.drive.DriveTrain;
+import org.montclairrobotics.sprocket.states.State;
 import org.montclairrobotics.sprocket.states.StateMachine;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -14,7 +15,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * the AutoStates that it contains before ending. When instantiated, these modes
  * will automatically be spent to a SmartDashboard chooser.
  */
-public class AutoMode {
+public class AutoMode extends StateMachine{
 	
 	private StateMachine machine;
 	private String name;
@@ -24,29 +25,19 @@ public class AutoMode {
 	 * @param name The name of the AutoMode (for SmartDashboard)
 	 * @param m The StateMachine which contains AutoModes
 	 */
-	public AutoMode(String name, StateMachine m)
+	public AutoMode(String name, State... states)
 	{
+		super(states);
 		this.name = name;
-		this.machine = m;
-	}
-
-	/**
-	 * Creates an AutoMode using AutoStates
-	 * @param name The name of the AutoMode (for SmartDasboard)
-	 * @param s A list of AutoModes which will run sequentially during the AutoMode
-	 */
-	public AutoMode(String name, AutoState... s)
-	{
-		this(name, new StateMachine(s));
 	}
 	public void start()
 	{
-		machine.start(true);
+		super.start(true);
 		SmartDashboard.putString("StartingAutoMode","START");
 	}
 	public void stop()
 	{
-		machine.stop();
+		super.stop();
 		SprocketRobot.getDriveTrain().useDefaultInput();
 	}
 	public String toString()
