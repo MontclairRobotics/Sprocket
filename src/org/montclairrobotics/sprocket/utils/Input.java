b99cc361d9dfbc44749481a1ba47b541d5d1527b
MHSRobotics <mhs@mpsdnj.us>
2017-03-29 15:53:46 -0400

    CHANGING THE MOST BASIC FUNCTIONALITY!!! YAAAYYYY!!!

--- src/org/montclairrobotics/sprocket/utils/Input.java
+++ src/org/montclairrobotics/sprocket/utils/Input.java
@@ -3,4 +3,14 @@ package org.montclairrobotics.sprocket.utils;
 public interface Input<T> {
 
     public T get();
+    
+    public static Input<Double> neg(Input<Double> a)
+    {
+    	return new Input<Double>(){
+
+			@Override
+			public Double get() {
+				return -a.get();
+			}};
+    }
 }
