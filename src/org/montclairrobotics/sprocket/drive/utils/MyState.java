6fb9d6c61bdc90d16afc7b93c8319ddadc750e6d
jackhymowitz <jack.hymowitz@gmail.com>
2017-06-24 18:23:30 -0400

    Get and Set

--- src/org/montclairrobotics/sprocket/drive/utils/MyState.java
+++ src/org/montclairrobotics/sprocket/drive/utils/MyState.java
@@ -5,10 +5,34 @@ import org.montclairrobotics.sprocket.geometry.Vector;
 import org.montclairrobotics.sprocket.utils.Input;
 
 public class MyState {
-	public Input<Vector>
+	private static Input<Vector>
 		location,
 		velocity;
-	public Input<Angle>
+	private static Input<Angle>
 		rotation,
 		rotationSpeed;
+	public static Vector getLocation() {
+		return location.get();
+	}
+	public static void setLocation(Input<Vector> location) {
+		MyState.location = location;
+	}
+	public static Vector getVelocity() {
+		return velocity.get();
+	}
+	public static void setVelocity(Input<Vector> velocity) {
+		MyState.velocity = velocity;
+	}
+	public static Angle getRotation() {
+		return rotation.get();
+	}
+	public static void setRotation(Input<Angle> rotation) {
+		MyState.rotation = rotation;
+	}
+	public static Angle getRotationSpeed() {
+		return rotationSpeed.get();
+	}
+	public static void setRotationSpeed(Input<Angle> rotationSpeed) {
+		MyState.rotationSpeed = rotationSpeed;
+	}
 }
