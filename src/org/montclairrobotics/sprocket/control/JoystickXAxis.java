9d5375aa610465c2276ab9b4894d690f3d0f5519
jackhymowitz <jack.hymowitz@gmail.com>
2017-06-27 22:48:07 -0400

    AAAAAAAAAAAnd here is another class to make up for the one I removed.

---
+++ src/org/montclairrobotics/sprocket/control/JoystickXAxis.java
@@ -0,0 +1,32 @@
+package org.montclairrobotics.sprocket.control;
+
+import org.montclairrobotics.sprocket.core.IJoystick;
+import org.montclairrobotics.sprocket.utils.Input;
+
+
+/**
+ * This class is a pretty simple wrapper around the Joystick Y-axis which just
+ * makes it into an Input{@literal <}Double{@literal >} for use with Sprocket
+ * classes such as ControlledMotor.
+ */
+public class JoystickXAxis implements Input<Double> {
+	
+	private IJoystick stick;
+
+
+	/**
+	 * @param stick The Joystick
+	 */
+	public JoystickXAxis(IJoystick stick) {
+		this.stick = stick;
+	}
+
+	/**
+	 * @return The Joystick's Y-axis
+	 */
+	@Override
+	public Double get() {
+		return stick.getX();
+	}
+
+}
