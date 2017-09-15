1526b21654cbae0ca4bffb96a9aeb7660ccfd7b8
jackhymowitz <jack.hymowitz@gmail.com>
2017-09-15 17:28:59 -0400

    UniversalMapper created; should work for symmetric drivetrains, including mecanum,

---
+++ src/org/montclairrobotics/sprocket/drive/UniversalMapper.java
@@ -0,0 +1,34 @@
+package org.montclairrobotics.sprocket.drive;
+
+import org.montclairrobotics.sprocket.geometry.Angle;
+import org.montclairrobotics.sprocket.geometry.Radians;
+import org.montclairrobotics.sprocket.geometry.Vector;
+
+public class UniversalMapper implements DTMapper{
+
+	@Override
+	public void map(DTTarget driveTarget, DriveModule[] driveModules) {
+		if(driveModules.length==0) return;
+		Vector dir=driveTarget.getDirection().setMag(1);
+		double maxForce=0;
+		double torque=0;
+		for(DriveModule module:driveModules)
+		{
+			double f=module.getForce().dotProduct(dir);
+			Vector fVec=module.getForce().setMag(f);
+			torque+=module.getOffset().rotate(Angle.QUARTER).setMag(1).dotProduct(fVec);
+			if(f>maxForce)
+				maxForce=f;
+		}
+		Angle tgtTurn=driveTarget.getTurn().subtract(new Radians(torque));
+		for(DriveModule module:driveModules)
+		{
+			double f=module.getForce().dotProduct(dir);
+			Vector fVec=module.getForce().setMag(f/maxForce);
+			Vector tVec=module.getOffset().rotate(Angle.QUARTER).setMag(torque);
+			Vector vec=fVec.add(tVec);
+			module.set(vec.dotProduct(module.getForce())/module.getForce().getMagnitude());
+		}
+	}
+
+}
