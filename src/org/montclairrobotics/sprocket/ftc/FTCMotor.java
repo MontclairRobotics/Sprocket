026fecc82a40fb0e84702bb2beb9cc0682cf06ac
jackhymowitz <jack.hymowitz@gmail.com>
2017-09-20 23:17:16 -0400

    Added run to position methods to FTCMotor

--- src/org/montclairrobotics/sprocket/ftc/FTCMotor.java
+++ src/org/montclairrobotics/sprocket/ftc/FTCMotor.java
@@ -7,17 +7,51 @@ import org.montclairrobotics.sprocket.core.IMotor;
 public class FTCMotor implements IMotor{
 
 	private DcMotor motor;
+	private double zeroPos=0;
 	
 	public FTCMotor(String motorID)
 	{
 		DcMotor motor=FTCRobot.ftcHardwareMap.dcMotor.get(motorID);
+		setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+	}
+	public FTCMotor setMode(DcMotor.RunMode mode)
+	{
+		motor.setMode(mode);
 		motor.setPower(0);
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+		return this;
+	}
+	public FTCMotor setZeroPowerBehavior(DcMotor.ZeroPowerBehavior b)
+	{
+		motor.setZeroPowerBehavior(b);
+		return this;
 	}
 	@Override
 	public void set(double power) {
 		motor.setPower(power);
+	}
+	public void setTargetPosition(double pos)
+	{
+		motor.setTargetPosition((int)(pos+0.5-zeroPos));
+	}
+	public void resetZeroPos()
+	{
+		zeroPos=getCurrentPos();
+	}
+	public void resetZeroPos(double currentVal)
+	{
+		zeroPos=getCurrentPosRaw()-currentVal;
+	}
+	public int getCurrentPosRaw()
+	{
+		return motor.getCurrentPosition();
+	}
+	public double getCurrentPos()
+	{
+		return motor.getCurrentPosition()-zeroPos;
+	}
+	public void setZeroPos(double zeroPos)
+	{
+		this.zeroPos=zeroPos;
 	}
 }
