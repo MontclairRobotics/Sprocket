7c16e3815dc82563160f90542458543bc9109134
Rafi Baum <rafi@ukbaums.com>
2017-11-02 09:29:17 -0400

    Mecanum mapper that should work

--- src/org/montclairrobotics/sprocket/drive/MecanumMapper.java
+++ src/org/montclairrobotics/sprocket/drive/MecanumMapper.java
@@ -1,49 +1,23 @@
 package org.montclairrobotics.sprocket.drive;
 
 
-import org.montclairrobotics.sprocket.geometry.Angle;
-import org.montclairrobotics.sprocket.geometry.Position;
+import org.montclairrobotics.sprocket.geometry.Degrees;
 import org.montclairrobotics.sprocket.geometry.Vector;
-import org.montclairrobotics.sprocket.geometry.XY;
 
 public class MecanumMapper implements DTMapper {
 
     @Override
     public void map(DTTarget driveTarget, DriveModule[] driveModules) {
+        Vector dir = driveTarget.getDirection();
+        double dirPower = dir.getMagnitude();
         double turn = driveTarget.getTurn();
 
-        for(DriveModule m : driveModules) {
-            double power = getPower(driveTarget.getDirection(), m.getForceAngle());
-
-            //Get vector for rotating
-            Vector turnVector;
-            if(m.getOffset() == Position.FL) {
-                turnVector = new XY(1, 1);
-            } else if(m.getOffset() == Position.FR) {
-                turnVector = new XY(1, -1);
-            } else if(m.getOffset() == Position.BL) {
-                turnVector = new XY(-1, 1);
-            } else if(m.getOffset() == Position.BR) {
-                turnVector = new XY(-1, -1);
-            } else {
-                turnVector = new XY(0, 0);
-            }
-
-            //Scale it to the appropriate turn speed
-            turnVector.scale(turn);
-            double turnPower = getPower(turnVector, m.getForceAngle());
-
-            power += turnPower;
-
-            //TODO: SCALING!!!!!!!
-            m.set(power);
+        for (DriveModule driveModule : driveModules) {
+            Vector offset = driveModule.getOffset();
+            Vector turnVec = offset.rotate(new Degrees(90)).normalize().scale(turn / (dirPower + Math.abs(turn)));
+            Vector finalVec = dir.normalize().scale(dirPower / (dirPower + Math.abs(turn))).add(turnVec);
+            driveModule.set(finalVec.dotProduct(driveModule.getForce()));
         }
-    }
-
-
-    private static double getPower(Vector vector, Angle forceAngle) {
-        // Power = tY*csc(angle) + tX*sec(angle)
-        return vector.getY() * (1/Math.sin(forceAngle.toRadians())) + vector.getX() * (1/Math.sin(forceAngle.toRadians()));
     }
 
 }
