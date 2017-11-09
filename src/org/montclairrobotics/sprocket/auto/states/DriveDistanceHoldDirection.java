0694673e59c6766038db9a2307d035f9183b12fb
jackhymowitz <jack.hymowitz@gmail.com>
2017-11-09 09:28:40 -0500

    Added a fun multistate - drive in a direction with gyrolock

---
+++ src/org/montclairrobotics/sprocket/auto/states/DriveDistanceHoldDirection.java
@@ -0,0 +1,11 @@
+package org.montclairrobotics.sprocket.auto.states;
+
+import org.montclairrobotics.sprocket.actions.MultiState;
+import org.montclairrobotics.sprocket.geometry.Vector;
+
+public class DriveDistanceHoldDirection extends MultiState{
+	public DriveDistanceHoldDirection(Vector direction,double power,double tgt,boolean relative)
+	{
+		super(0,new DriveDistance(direction, power),new TurnGyro(tgt,relative));
+	}
+}
