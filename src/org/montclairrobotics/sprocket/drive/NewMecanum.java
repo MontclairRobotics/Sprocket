a06112a8b4145a219481d1fe4f406b0c75658bc3
MHSRobotics <mhs@mpsdnj.us>
2017-11-11 16:04:09 -0500

    NewMecanum Mapper

---
+++ src/org/montclairrobotics/sprocket/drive/NewMecanum.java
@@ -0,0 +1,33 @@
+package org.montclairrobotics.sprocket.drive;
+
+import org.montclairrobotics.sprocket.geometry.Degrees;
+import org.montclairrobotics.sprocket.geometry.Vector;
+
+/**
+ * Created by MHS Robotics on 11/11/2017.
+ */
+
+public class NewMecanum {
+    public void map(DTTarget driveTarget, DriveModule[] driveModules) {
+        //Setting up variables
+        double x = driveTarget.getDirection().getX();
+        double y = driveTarget.getDirection().getY();
+        double turn = driveTarget.getTurn();
+        double maxPower=1;
+
+        for(DriveModule module:driveModules)
+        {
+            double xSign=module.getOffset().getX()>0?1:-1;
+            double ySign=module.getOffset().getY()>0?1:-1;
+            double dirSign=module.getOffset().crossProduct(module.getForce())>0?1:-1;
+            module.temp=(y*xSign*-1+x*ySign+turn)*dirSign;
+            if(module.temp>maxPower)
+            {
+                maxPower=module.temp;
+            }
+        }
+        for(DriveModule module:driveModules) {
+            module.set(module.temp/maxPower);
+        }
+    }
+}
