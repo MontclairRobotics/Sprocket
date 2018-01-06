9612394e669a4675609ba5966568d3c88f5006f1
Rafi Baum <rafi@ukbaums.com>
2018-01-06 00:06:50 -0500

    Added documentation for Motor

--- src/org/montclairrobotics/sprocket/motors/Motor.java
+++ src/org/montclairrobotics/sprocket/motors/Motor.java
@@ -7,6 +7,10 @@ import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 
+/**
+ * Motor is a Sprocket class which wraps around WPILib SpeedController classes, making WPILib motors compatible
+ * with Sprocket classes.
+ */
 public class Motor {
 
     public enum MotorType {
@@ -23,8 +27,11 @@ public class Motor {
     private double minPower = -1.0;
 	private double maxPower = 1.0;
 	private boolean brakeMode=true;
-	
 
+    /**
+     * Instantiates a motor based off of a WPILib SpeedController.
+     * @param motor A WPILib motor object (must inherit SpeedController).
+     */
     public Motor(SpeedController motor) {
         if(motor == null) {
             throw new IllegalArgumentException("SpeedController argument was null when instantiating Motor object");
@@ -53,8 +60,11 @@ public class Motor {
         }
 
     }
-    
-    
+
+    /**
+     * Sets the power of the motor to a value from -1 to 1.
+     * @param power The power of the motor (in percentage) from -1 to 1.
+     */
     public void set(double power) {
     	if(brakeMode&&Math.abs(power)<0.05)
     	{
@@ -66,22 +76,41 @@ public class Motor {
     	}
     }
 
+    /**
+     * @return If the motor has been inverted or not
+     */
     public boolean getInverted() {
         return motor.getInverted();
     }
 
+    /**
+     * Negates the motor power that is set if convenient.
+     * @param invert If the motor is inverted or not.
+     */
     public void setInverted(boolean invert) {
         motor.setInverted(invert);
     }
 
+    /**
+     * @return The native WPILib object which this class wraps.
+     */
     public SpeedController getMotor() {
         return motor;
     }
 
+    /**
+     * @return The type of motor controller this class represents.
+     */
     public MotorType getMotorType() {
         return motorType;
     }
-    
+
+    /**
+     * Constrains the power values of this motor within a certain minimum and maximum.
+     * @param min The minimum power which the motor can run at.
+     * @param max The maximum power which the motor can run at.
+     * @return
+     */
     public Motor constrain(double min, double max) {
 		this.minPower = min;
 		this.maxPower = max;
