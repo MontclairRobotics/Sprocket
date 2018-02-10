f24b469c5d357e3b8c847d898baaf208426759fc
Joshua Rapoport <j.wish.2012@gmail.com>
2018-02-10 13:11:46 -0500

    Created `Event.java`, a subclass of `Action` that monitors a given instance of `Input<Double>`. To create one, use a class (static) method: - `Event.equalsTarget(double, Input<Double>)` - `Event.equalsTargetWithError(double, double, Input<Double>)` - `Event.aboveTarget(double,Input<Double>)` - `Event.belowTarget(double,Input<Double>)`

--- src/org/montclairrobotics/sprocket/jrapoport/Event.java
+++ src/org/montclairrobotics/sprocket/jrapoport/Event.java
@@ -1,7 +1,47 @@
 package org.montclairrobotics.sprocket.jrapoport;
 
 import org.montclairrobotics.sprocket.utils.Input;
+import org.montclairrobotics.sprocket.utils.Range;
 
-public class Event {
+public class Event extends Action {
+	private Input<Double> input;
+	private double lastInput;
+	private Range range;
+	
+	protected Event(Range range, Input<Double> input) {
+		this.input = input;
+		this.range = range;
+	}
+	
+	@Override
+	public void start() {
+		this.lastInput = input.get().doubleValue();
+	}
 
+	@Override
+	public void update() {
+		lastInput = input.get().doubleValue();
+	}
+
+	@Override
+	public boolean isComplete() { return range.contains(lastInput); }
+
+	@Override
+	public void stop() {}
+	
+	public final static Event equalsTarget(double t, Input<Double> in) {
+		return new Event(new Range(t - 1e-10, t + 1e+10), in);
+	}
+	
+	public final static Event equalsTargetWithError(double t, double e, Input<Double> in) {
+		return new Event(new Range(t - e, t + e), in);
+	}
+	
+	public final static Event aboveTarget(double t, Input<Double> in) {
+		return new Event(new Range(t, Double.MAX_VALUE), in);
+	}
+	
+	public final static Event belowTarget(double t, Input<Double> in) {
+		return new Event(new Range(-Double.MAX_VALUE, t), in);
+	}
 }
