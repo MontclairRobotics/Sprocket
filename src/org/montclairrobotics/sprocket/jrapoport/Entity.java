1d08d74c50a9e6b6135e76b18e070b190a77e9c7
Joshua Rapoport <j.wish.2012@gmail.com>
2018-02-15 16:06:16 -0500

    Updated `Entity.java`: no longer implements `List<Component>`, but it does manage is components using some methods.

--- src/org/montclairrobotics/sprocket/jrapoport/Entity.java
+++ src/org/montclairrobotics/sprocket/jrapoport/Entity.java
@@ -1,13 +1,9 @@
 package org.montclairrobotics.sprocket.jrapoport;
 
-import java.util.List;
-import java.util.ListIterator;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 
-public abstract class Entity implements Updatable, Component, List<Component> {
-	private String name;
+public abstract class Entity extends Component implements Updatable {
+	private final String name;
 	private ArrayList<Component> components;
 	
 	Entity(String name, Component... components) {
@@ -28,6 +24,15 @@ public abstract class Entity implements Updatable, Component, List<Component> {
 		}
 	}
 	
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+	public ArrayList<Component> getComponents() {
+		return components;
+	}
+	
 	/**
 	 * This is where this entity updates its components.
 	 * Override this class to perform updates specific to this entity.
@@ -39,83 +44,33 @@ public abstract class Entity implements Updatable, Component, List<Component> {
 		}
 	}
 	
-	/* =================== */
-	/* interface Component */
-	/* =================== */
+	public int length() { return components.size(); }
 	
-	@Override
-	public String getName() { return name; }
-
-	/* ========================= */
-	/* interface List<Component> */
-	/* ========================= */
-	
-	@Override
-	public int size() { return components.size(); }
-	
-	@Override
 	public boolean isEmpty() { return components.isEmpty(); }
 
-	@Override
-	public boolean contains(Object o) { return components.contains(o); }
+	public boolean contains(Component component) { return components.contains(component); }
 
-	@Override
-	public Iterator<Component> iterator() { return components.iterator(); }
+	public boolean add(Component component) { return components.add(component); }
 
-	@Override
-	public Object[] toArray() { return components.toArray(); }
+	public boolean remove(Component component) { return components.remove(component); }
+	
+	public void clearComponents() { components.clear(); }
 
-	@Override
-	public <T> T[] toArray(T[] a) { return components.toArray(a); }
-
-	@Override
-	public boolean add(Component e) { return components.add(e); }
-
-	@Override
-	public boolean remove(Object o) { return components.remove(o); }
-
-	@Override
-	public boolean containsAll(Collection<?> c) { return components.containsAll(c); }
-
-	@Override
-	public boolean addAll(Collection<? extends Component> c) { return components.addAll(c); }
-
-	@Override
-	public boolean addAll(int index, Collection<? extends Component> c) { return components.addAll(index, c); }
-
-	@Override
-	public boolean removeAll(Collection<?> c) { return components.removeAll(c); }
-
-	@Override
-	public boolean retainAll(Collection<?> c) { return components.retainAll(c); }
-
-	@Override
-	public void clear() { components.clear(); }
-
-	@Override
-	public Component get(int index) { return components.get(index); }
-
-	@Override
-	public Component set(int index, Component element) { return components.set(index, element); }
-
-	@Override
-	public void add(int index, Component element) { components.add(index, element); }
-
-	@Override
-	public Component remove(int index) {	return components.remove(index); }
-
-	@Override
-	public int indexOf(Object o) { return components.indexOf(o); }
-
-	@Override
-	public int lastIndexOf(Object o) { return components.lastIndexOf(o); }
-
-	@Override
-	public ListIterator<Component> listIterator() { return components.listIterator(); }
-
-	@Override
-	public ListIterator<Component> listIterator(int index) { return components.listIterator(index); }
-
-	@Override
-	public List<Component> subList(int fromIndex, int toIndex) { return components.subList(fromIndex, toIndex); }
+	public Component getComponent(String name) {
+		for (Component c : components ) {
+			if (false) //TODO implement Identifiable
+				return c;
+		}
+		
+		return null;
+	}
+	
+	public boolean removeComponent(String name) {
+		for (Component c : components ) {
+			if (false) //TODO implement Identifiable
+				return components.remove(c);
+		}
+		
+		return false;
+	}
 }
