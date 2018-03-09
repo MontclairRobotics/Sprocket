cfd5790e5da52502306a243930e0cc0de59e7404
Jack Hymowitz <Jack.hymowitz@gmail.com>
2018-03-08 20:06:12 -0500

    Copied and pasted from the older monitor, still here to test.

--- src/main/java/org/montclairrobotics/sprocket/motors/Motor.java
+++ src/main/java/org/montclairrobotics/sprocket/motors/Motor.java
@@ -2,6 +2,9 @@ package org.montclairrobotics.sprocket.motors;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import org.montclairrobotics.sprocket.utils.Debug;
+import org.montclairrobotics.sprocket.utils.Input;
 import org.montclairrobotics.sprocket.utils.Utils;
 
 import edu.wpi.first.wpilibj.SpeedController;
@@ -15,17 +18,25 @@ public class Motor {
         UNKNOWN
     }
     
-    
-
+    private String name;
+    private Input<Double> current;
     private SpeedController motor;
     private MotorType motorType;
     
     private double minPower = -1.0;
 	private double maxPower = 1.0;
 	private boolean brakeMode=true;
+	private boolean eStop=false;
+	
+	private static boolean globalEStop=false;
 	
 
     public Motor(SpeedController motor) {
+    	this(null,motor,null);
+    }
+    	
+    public Motor(String name, SpeedController motor, Input<Double> current)
+    {
         if(motor == null) {
             throw new IllegalArgumentException("SpeedController argument was null when instantiating Motor object");
         }
@@ -50,21 +61,44 @@ public class Motor {
             default:
             break;
         }
+        
+        this.name=name;
+        this.current=current;
 
     }
     
     
     public void set(double power) {
-    	if(brakeMode&&Math.abs(power)<0.05)
+    	double val=0.0;
+    	if(globalEStop ||eStop)
+    	{
+    		motor.set(0);
+    	}
+    	else if(brakeMode&&Math.abs(power)<0.05)
     	{
     		motor.stopMotor();
     	}
     	else
     	{
-    		motor.set(Utils.constrain(power, minPower, maxPower));
+    		val=Utils.constrain(power, minPower, maxPower);
+    		motor.set(val);
+    	}
+    	if(name!=null&&current!=null)
+    	{
+    		Debug.msg("Motor "+name,"Power: "+String.format("%.2f",val)+", Current: "+String.format("%.2f",current.get()));
     	}
     }
 
+    public void eStop()
+    {
+    	eStop=true;
+    }
+    
+    public void globalEStop()
+    {
+    	globalEStop=true;
+    }
+    
     public boolean getInverted() {
         return motor.getInverted();
     }
